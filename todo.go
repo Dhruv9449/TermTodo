d5e0bd119d9f676926112077c15c486e822acfc2
Dhruv Shah <dhruvshahrds@gmail.com>
2023-02-05 02:53:29 +0530

    docs: edited the help page

--- todo.go
+++ todo.go
@@ -30,6 +30,29 @@ func main() {
 	// Create a new cli app
 	app := cli.NewApp()
 
+	cli.AppHelpTemplate = "  _______               _______        _       \n |__   __|             |__   __|      | |      \n    | | ___ _ __ _ __ ___ | | ___   __| | ___  \n    | |/ _ \\ '__| '_ ` _ \\| |/ _ \\ / _` |/ _ \\ \n    | |  __/ |  | | | | | | | (_) | (_| | (_) |\n    |_|\\___|_|  |_| |_| |_|_|\\___/ \\__,_|\\___/ \n" +
+		`
+A very simple CLI todo app written in Go!
+
+Developer :
+	Dhruv Shah (https://github.com/Dhruv9449)
+
+For more detailed help:
+	https://github.com/Dhruv9449/TermTodo/blob/main/README.md
+		
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+   {{if len .Authors}}
+VERSION:
+   {{.Version}}
+   {{end}}{{if .Commands}}
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}
+`
+
 	// Set the app details
 	app.Name = "TermTodo"
 	app.Usage = "A simple todo app for the terminal"
